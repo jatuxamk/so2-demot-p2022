0f57e867fe0f3730f9501241ea7823d0fd7120e5
Janne Turunen <janne.turunen@xamk.fi>
2022-01-17 12:06:28 +0200

    Demo 2: Web Service -perusteita

--- demo02/index.ts
+++ demo02/index.ts
@@ -1,13 +1,73 @@
 import express from 'express';
+import path from 'path';
 import kayttajat from "./kayttajat.json";
+
+interface Kayttaja {
+    id: number;
+    sukunimi: string;
+    etunimi: string;
+    sukupuoli: string;
+    kaupunki: string;
+    maa: string;
+    sahkoposti: string;
+    ipOsoite: string;
+    luottokorttityyppi: string;
+    luottokortinNumero: string;
+    kayttajatunnus: string;
+    salasana: string;
+}
+
+interface Data {
+    id: number,
+    nimi : string,
+    kayttajatunnus : string,
+    sahkoposti : string,
+    maa : string
+}
 
 const app : express.Application = express();
 
 const portti : number = Number(process.env.PORT) || 3002;
 
-app.get("/", (req : express.Request, res : express.Response) => {
+app.use(express.static(path.resolve(__dirname, "public")));
 
-    res.json(kayttajat);
+app.get("/kayttajat/:id", (req : express.Request, res : express.Response) => {
+
+    let lahetettavaData : Kayttaja = kayttajat.find((kayttaja : Kayttaja) => {
+
+        return kayttaja.id === Number(req.params.id);
+
+    });
+
+    res.json(lahetettavaData)
+
+});
+
+app.get("/kayttajat", (req : express.Request, res : express.Response) => {
+
+    let lahetettavaData : Data[] = kayttajat.map((kayttaja : Kayttaja) => {
+
+        return {
+                id : kayttaja.id,
+                nimi : `${kayttaja.sukunimi} ${kayttaja.etunimi}`,
+                sahkoposti : kayttaja.sahkoposti,
+                kayttajatunnus : kayttaja.kayttajatunnus,
+                maa : kayttaja.maa
+        }
+
+    });
+
+    if (typeof req.query.maa === "string") {
+
+        lahetettavaData = lahetettavaData.filter((data : Data) => {
+
+            return data.maa === req.query.maa;
+
+        })
+
+    }
+
+    res.json(lahetettavaData);
 
 });
 
