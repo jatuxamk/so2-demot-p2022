b49eb11dba4096f9b00e6a61a41ae7bed71286ca
Janne Turunen <janne.turunen@xamk.fi>
2022-01-24 11:48:33 +0200

    Demo 4 : REST API (2)

--- demo04/routes/api.ts
+++ demo04/routes/api.ts
@@ -1,27 +1,47 @@
 import express from 'express';
+import ostokset, { Ostos } from '../models/ostokset';
 
 const apiRouter : express.Router = express.Router();
 
 apiRouter.use(express.json()); // Otetaan vastaan mahdollinen pyynnön body json-datana
 
+
+apiRouter.post("/api/ostokset", async (req : express.Request, res : express.Response) : Promise<void> => {
+
+
+    let vanhatOstokset : Ostos[] = await ostokset.haeKaikki();
+
+    let uusiOstos : Ostos = {
+                        id : vanhatOstokset[vanhatOstokset.length -1].id + 1,
+                        ostos : req.body.ostos
+                    }
+
+    vanhatOstokset = [...vanhatOstokset, uusiOstos];
+
+    //tallennaOstokset(ostokset);
+
+    res.json({ "viesti" : "Uusi ostos lisätty" })
+
+});
+
 apiRouter.get("/ostokset", async (req : express.Request, res : express.Response) : Promise<void> => {
 
-    /*
 
-    let ostokset : Ostos[] = await haeOstokset();
+    try {
 
-    if (ostokset) {
+        let data : Ostos[] = await ostokset.haeKaikki();
 
-        res.json(ostokset);
+        res.json(data);
 
-    } else {
+    } catch (e : any) {
 
-        res.json({ "virhe" : "Palvelinvirhe" })
+        console.log(e)
+
+        res.status(e.status).json({ "virhe" : e.teksti })
+
     }
-    */
-
-    res.json("testi");
-
+              
+ 
 });
 
 export default apiRouter;
