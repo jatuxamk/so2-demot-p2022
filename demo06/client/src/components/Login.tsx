e0946a0ef8b66acb46cae6c4c2a3fc79ca211249
Janne Turunen <janne.turunen@xamk.fi>
2022-02-07 12:29:00 +0200

    Demo 6: Ostoslista (JWT-autorisointi)

--- demo06/client/src/components/Login.tsx
+++ demo06/client/src/components/Login.tsx
@@ -1,14 +1,10 @@
 import React, { useRef, SetStateAction, Dispatch } from "react";
 import { Box, Button, Container, Stack, TextField, Typography} from "@mui/material";
-import { useNavigate } from "react-router-dom";
-
 
 const Login: React.FC = () : React.ReactElement => {
 
     const tunnusInput = useRef<HTMLInputElement>();
     const salasanaInput = useRef<HTMLInputElement>();
-
-    const navigate = useNavigate();
 
     const kirjaudu = async (e : React.FormEvent) : Promise<void> => {
         
